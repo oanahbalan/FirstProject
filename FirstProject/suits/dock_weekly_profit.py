d2e0813ce6eb266446ea296381b6b87b5d0c7338
oanahbalan <oanah.balan@gmail.com>
2025-02-09 17:14:49 +0200

    Add files via upload

---
+++ FirstProject/suits/dock_weekly_profit.py
@@ -0,0 +1,9 @@
+import os
+import sys
+
+sys.path.append((os.path.dirname(os.getcwd())))  # Append path to dock_daily_profit
+
+import dock_weekly_profit as utils
+
+if __name__ == "__main__":
+    utils.dock_management()
